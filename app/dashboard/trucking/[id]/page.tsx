5e897cbe7b130b227892346338650c2bad695ed9
raschlabach <104387277+raschlabach@users.noreply.github.com>
2025-11-19 17:14:59 -0500

    details card height fix

--- app/dashboard/trucking/[id]/page.tsx
+++ app/dashboard/trucking/[id]/page.tsx
@@ -128,9 +128,9 @@ export default function TruckloadBuilderPage({ params }: TruckloadBuilderPagePro
                         <div className="w-80 flex-none border-r h-full overflow-hidden">
                             <TruckloadSidebarList truckloadId={truckloadId} />
                         </div>
-                        <div className="flex-grow overflow-hidden min-h-0">
-                            <Card className="h-full flex flex-col">
-                                <Tabs defaultValue="stops" className="h-full flex flex-col">
+                        <div className="flex-grow overflow-hidden min-h-0 flex flex-col">
+                            <Card className="flex-1 flex flex-col min-h-0 overflow-hidden">
+                                <Tabs defaultValue="stops" className="flex-1 flex flex-col min-h-0 overflow-hidden">
                                     <div className="border-b px-4 flex-shrink-0">
                                         <TabsList>
                                             <TabsTrigger value="stops">Stops</TabsTrigger>
@@ -140,24 +140,24 @@ export default function TruckloadBuilderPage({ params }: TruckloadBuilderPagePro
                                         </TabsList>
                                     </div>
 
-                                    <TabsContent value="stops" className="flex-1 min-h-0 overflow-hidden flex flex-col">
+                                    <TabsContent value="stops" className="flex-1 min-h-0 overflow-hidden flex flex-col mt-0">
                                         <TruckloadStopsList truckloadId={truckloadId} />
                                     </TabsContent>
 
-                                    <TabsContent value="map" className="flex-grow">
-                                        <div className="h-full">
+                                    <TabsContent value="map" className="flex-1 min-h-0 overflow-hidden flex flex-col mt-0">
+                                        <div className="h-full min-h-0">
                                             <TruckloadMap truckloadId={truckloadId} />
                                         </div>
                                     </TabsContent>
 
-                                    <TabsContent value="load-builder" className="flex-grow">
-                                        <div className="h-full">
+                                    <TabsContent value="load-builder" className="flex-1 min-h-0 overflow-hidden flex flex-col mt-0">
+                                        <div className="h-full min-h-0">
                                             <TruckloadLoadBuilder truckloadId={truckloadId} />
                                         </div>
                                     </TabsContent>
 
-                                    <TabsContent value="load-papers" className="flex-grow">
-                                        <div className="h-full">
+                                    <TabsContent value="load-papers" className="flex-1 min-h-0 overflow-hidden flex flex-col mt-0">
+                                        <div className="h-full min-h-0">
                                             <TruckloadLoadPapers truckloadId={truckloadId} />
                                         </div>
                                     </TabsContent>
